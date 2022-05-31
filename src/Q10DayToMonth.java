5c7802f07b09bc064d662cc8c06c95538441de6e
Alok Sarkar <aloksarkarxxxx@gmail.com>
2022-05-31 21:29:03 +0530

    [Add] Days to month program

---
+++ src/Q10DayToMonth.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q10DayToMonth {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number of days: ");
+        int days=sc.nextInt();
+        System.out.println(days+" days = "+days/30+" months & "+days%30+" days");
+    }
+}
