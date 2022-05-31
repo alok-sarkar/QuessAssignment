eed29c72c8e7309fb61cfc807b6280149f06ad61
Alok Sarkar <aloksarkarxxxx@gmail.com>
2022-05-31 21:24:56 +0530

    [Add] Commandline discount program

---
+++ src/Q9CmdlineDiscount.java
@@ -0,0 +1,16 @@
+public class Q9CmdlineDiscount {
+    public static void main(String[] args) {
+        if (args.length>1) {
+            double price=Double.parseDouble(args[0]);
+            int quantity=Integer.parseInt(args[1]);
+            double bill=price*quantity;
+            if(quantity>=10){
+                bill-=(bill*.1);
+            }
+            bill=(double) Math.round(bill * 100) / 100;
+            System.out.println("Final amount: "+bill);
+        }else{
+            System.out.println("Insufficient argument");
+        }
+    }
+}
