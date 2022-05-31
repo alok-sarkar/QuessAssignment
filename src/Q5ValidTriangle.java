66d699e7e85c7f48abba40a416e19dcb9c3c145b
Alok Sarkar <aloksarkarxxxx@gmail.com>
2022-05-31 17:26:56 +0530

    [Add] Valid triangle program

---
+++ src/Q5ValidTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q5ValidTriangle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter 1st angle : ");
+        int deg1=sc.nextInt();
+        System.out.print("Enter 2nd angle : ");
+        int deg2=sc.nextInt();
+        System.out.print("Enter 3rd angle : ");
+        int deg3=sc.nextInt();
+        if(deg1+deg2+deg3==180)
+            System.out.println("It is a triangle");
+        else
+            System.out.println("It is not a triangle");
+    }
+}
