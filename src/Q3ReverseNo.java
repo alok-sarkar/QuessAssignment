1d1346106e9cae32238a50e7b86162c06ad192e4
Alok Sarkar <aloksarkarxxxx@gmail.com>
2022-05-31 17:12:25 +0530

    [Add] Reverse 3-digit number program

---
+++ src/Q3ReverseNo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Q3ReverseNo {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a three digit number : ");
+        int num=sc.nextInt();
+        //We Could Use The String builder
+        StringBuilder str=new StringBuilder(Integer.toString(num)).reverse();
+        System.out.println("Reversed value{String method}: "+str);
+        // or the Manual by-division method
+        int result=(num%10)*100;
+        num/=10;
+        result+=(num%10)*10;
+        num/=10;
+        result+=(num%10);
+        System.out.println("Reversed value{integer method}: "+result);
+    }
+}
