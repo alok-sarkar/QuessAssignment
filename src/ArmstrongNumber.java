3eb1c23add75eab3f2cdb75e36be8340ff4d1f4c
Alok Sarkar <aloksarkarxxxx@gmail.com>
2022-05-31 13:10:01 +0530

    [Add] Armstrong Number program

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+        int count=0,temp=0,dup=num;
+        while (dup!=0){
+            temp+=Math.pow((dup%10),3);
+            dup/=10;
+        }
+        if(temp==num){
+            System.out.println("Armstrong number");
+        }else
+            System.out.println("Not a Armstrong number");
+
+    }
+}
