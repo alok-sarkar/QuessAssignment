48942775b4fa796c4ca292e4994ca8637ada65af
Alok Sarkar <aloksarkarxxxx@gmail.com>
2022-05-31 17:02:43 +0530

    [Add] Simple Interest program

---
+++ src/Q2SimpleInterest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q2SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter principle amount: ");
+        double amount=sc.nextDouble();
+        System.out.print("Enter number of years: ");
+        int years=sc.nextInt();
+        double interest=((amount*7.5)/100)*years;
+        System.out.println("Simple Interest: "+interest);
+        System.out.println("Total amount: "+(amount+interest));
+    }
+}
