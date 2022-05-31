6f7205be6126593a6b7de798532640f83413a9fc
Alok Sarkar <aloksarkarxxxx@gmail.com>
2022-05-31 17:01:57 +0530

    [Add] Factorial from Commandline args

---
+++ src/CommandLineFactorial.java
@@ -0,0 +1,12 @@
+public class CommandLineFactorial {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            int n = Integer.parseInt(arg);
+            int val = 1;
+            for (int i = 1; i <= n; i++) {
+                val *= i;
+            }
+            System.out.println("factorial of " + arg + " is " + val);
+        }
+    }
+}
