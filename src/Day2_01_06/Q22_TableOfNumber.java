08846fd9ee5d445608b8ab71f8b094442e96497c
Alok Sarkar <aloksarkar0002@gmail.com>
2022-06-01 15:07:46 +0530

    [Add] Table of given number program

---
+++ src/Day2_01_06/Q22_TableOfNumber.java
@@ -0,0 +1,14 @@
+package Day2_01_06;
+
+import java.util.Scanner;
+
+public class Q22_TableOfNumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+        for (int i = 1; i<= 10; i++) {
+            System.out.println(num+" X "+i+" = "+num*i);
+        }
+    }
+}
