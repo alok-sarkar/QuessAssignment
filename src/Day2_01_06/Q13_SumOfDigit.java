ceddd59aab9a58d64f42afbe6c0d88f8dbc2f846
Alok Sarkar <aloksarkar0002@gmail.com>
2022-06-01 12:44:10 +0530

    [Add] Sum Of digit program

---
+++ src/Day2_01_06/Q13_SumOfDigit.java
@@ -0,0 +1,17 @@
+package Day2_01_06;
+
+import java.util.Scanner;
+
+public class Q13_SumOfDigit {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+        int sum=0;
+        while (num>0){
+            sum+=num%10;
+            num/=10;
+        }
+        System.out.println("Sum: "+sum);
+    }
+}
