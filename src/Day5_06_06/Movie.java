96e755c262100360ba1eb2787b21ffb619334ad1
Alok Sarkar <aloksarkar0002@gmail.com>
2022-06-06 16:38:37 +0530

    [ADD] Creating Movie class for Q_38(MovieMaker)

---
+++ src/Day5_06_06/Movie.java
@@ -0,0 +1,8 @@
+package Day5_06_06;
+
+public class Movie {
+    private String title;
+    private String studio;
+    private String rating;
+}
+
---
+++ src/Day5_06_06/Q38_MovieMaker.java
@@ -0,0 +1,19 @@
+package Day5_06_06;
+/*
+* The class Movie is started below. An instance of class Movie represents a film. This class has the following three class variables:
+title, which is a String representing the title of the movie
+studio, which is an String representing the studio that made the movie
+rating, which is a String representing the rating of the movie (i.e. PG-13, R, etc)
+
+    public class Movie {
+    private String title;
+    private String studio;
+    private String rating;
+    // your code goes here
+    }
+* */
+public class Q38_MovieMaker {
+    public static void main(String[] args) {
+
+    }
+}
