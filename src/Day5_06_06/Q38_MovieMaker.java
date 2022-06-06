766bd98f1929fc7865956e837f4f160b010e3b27
Alok Sarkar <aloksarkar0002@gmail.com>
2022-06-06 17:13:22 +0530

    [UPDATE] {Req:4} Creating object of Movie-class for Q_38(MovieMaker)

--- src/Day5_06_06/Q38_MovieMaker.java
+++ src/Day5_06_06/Q38_MovieMaker.java
@@ -17,9 +17,12 @@ rating, which is a String representing the rating of the movie (i.e. PG-13, R, e
 * Req 2: Write a second constructor for the class Movie, which takes a String representing the title of the movie and a String representing the studio as its arguments, and sets the respective class variables to these values, while the class variable rating is set to “PG”.
 *
 * Req3:Write a method getPg, which takes an array of base type Movie as its argument, and returns a new array of only those movies in the input array with a rating of “PG”. You may assume that the input array is full of Movie instances. The returned array need not be full.
+*
+* Req 4:Write a piece of code that creates an instance of the class Movie with the title “Casino Royale”, the studio “Eon Productions”, and the rating “PG-13”.
 * */
 public class Q38_MovieMaker {
     public static void main(String[] args) {
+        Movie movie =new Movie( "Casino Royale", "Eon Productions", "PG-13");
 
     }
 }
